3e657f706eac3d03292ce8ef671cb4d486165653
RizkiRa98 <rizkiramadhan350@gmail.com>
2023-02-08 10:21:44 +0700

    Yargs List

---
+++ index.js
@@ -0,0 +1,55 @@
+const yargs = require("yargs");
+const fungsiPertanyaan = require('./fungsiPertanyaan');
+const { exit } = require("process");
+
+yargs.command({
+  command:'add',
+  describe: 'add new contact',
+  builder:{
+    name:{
+      describe: 'Contact Name',
+      demandOption: true,
+      type: 'string',
+    },
+    email: {
+      describe: 'contact email',
+      demandOption: false,
+      type: 'string',
+    },
+    mobile:{
+      describe: 'Contact mobile phone number',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv){
+    fungsiPertanyaan.initData();
+    fungsiPertanyaan.saveJawaban(argv.name, argv.email, argv.mobile);
+  },  
+})
+.command({
+  command: 'detail',
+  describe: 'Detail Contact',
+  builder:{
+    name:{
+      describe: 'Contact Name',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv){
+  fungsiPertanyaan.initData();
+    fungsiPertanyaan.readData(argv.name);
+  },
+})
+//command memanggil listData
+.command({
+  command: 'list',
+  describe: 'List Contact',
+handler(argv){
+  fungsiPertanyaan.listData();
+}
+});
+
+//yargs.parse() untuk memunculkan perintah dari yargs
+yargs.parse();
